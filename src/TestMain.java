90f9ab309611ad6f9e59aee9dbab75f445767076
billy0219 <billy0219@gmail.com>
2017-07-04 16:57:12 +0900

    first commit

---
+++ src/TestMain.java
@@ -0,0 +1,5 @@
+/**
+ * Created by billy on 2017-06-29.
+ */
+public class TestMain {
+}
---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by billy on 2017-06-30.
+ */
+public class main {
+    public static void main(String[] args){
+        // System.out.println("hello");
+
+        // operator
+        Operators operators = new Operators();
+        operators.calculate();
+    }
+}
